3372b678b40041091082cb0a0ae69afe23480856
yangyuexiong <z87254091>
2021-05-31 17:01:41 +0800

    command_register

---
+++ ExtendRegister/model_register.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/5/31 上午10:37
+# @Author  : ShaHeTop-Almighty-ares
+# @Email   : yang6333yyx@126.com
+# @File    : model_register.py
+# @Software: PyCharm
+
+from app.models.admin.models import Admin, Role, Permission, MidAdminAndRole, MidPermissionAndRole, ApiResource, \
+    RouteResource
+from app.models.product.models import ProductBrand, ProductCategory, Product, Sku, AttributeKey, AttributeVal, \
+    ProductComment, ProductStock, ProductSales, ProductOperationLog
+from app.models.order.models import Order
